6d9067a50bc2155bb9383e63667973bfc8ed92ea
kileysel <kiley@selmanhouse.com>
2018-06-15 19:41:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,28 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/index' do
+    erb :index
+  end
+  get '/happy' do
+    erb :happy
+  end
+  get '/upset' do 
+    erb :upset
+  end
+  get '/peaceful' do 
+    erb :peaceful
+  end
+  get '/stressed' do 
+    erb :stressed
+  end
+  get '/about_us' do
+    erb :about_us
+  end
 
 end
